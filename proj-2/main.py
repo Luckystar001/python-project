feb0ca8be98626ce09779043eee9276c42371b4c
Abah O. Lucky <abahoche30@gmail.com>
2024-10-20 20:20:19 +0100

    Mathematical Operations

--- proj-2/main.py
+++ proj-2/main.py
@@ -1,6 +1,21 @@
-print ("This is a test")
+# Mathematical operations
+import math
+x = -4
+y = 3.2
+z = 12
 
-a = 5
-b = 4
+# result = abs(x)
+# result = round(y)
 
-print (a + b)
+# result = pow(z, 2)
+result = math.sqrt(z)
+
+
+print(result)
+
+radius = float(input("Enter the radius of a circle:"))
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius, 2)
+area = round(area, 2)
+print(f"The circumference of the circle is {circumference}")
+print(f"The area of the circle is {area}")
