2d0cd5b1553d8442e2faeb8a5f50f9c7bccc1f59
Abah O. Lucky <abahoche30@gmail.com>
2024-10-20 03:17:49 +0100

    Variables, Typecasting and input field

---
+++ proj-1/main.py
@@ -0,0 +1,28 @@
+# Variables
+
+first_name = "Lucky"
+last_name = "Abah"
+food = "Bread"
+age = 25
+
+print (f"Hi My name is {first_name} {last_name}")
+print (f" I am {age} years old.")
+print (f"I love eating {food}")
+
+#Typecasting
+#process of converting variabes from one data type to another
+name = "Abah Lucky"
+age = 25
+cgpa =3.03
+a_student = True
+
+age = float(age)
+print(age) 
+
+cgpa = int(cgpa)
+print (cgpa)
+
+# input() a function that prompts users to enter data, it returns the data as a string
+name = input("What is your name:")
+
+print(f"My name is {name}")
