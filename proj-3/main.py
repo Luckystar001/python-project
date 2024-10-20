6d1ea1fc56811249bd7e32af70d083095a00ce50
Abah O. Lucky <abahoche30@gmail.com>
2024-10-20 23:25:30 +0100

    if statements

---
+++ proj-3/main.py
@@ -0,0 +1,12 @@
+# if statements
+# if - do something if the condition is true
+# Else- do somethingelse
+
+age = int(input("What is your age: "))
+
+if age <= 10:
+    print("you are a child")
+elif age <= 30: 
+    print ("You are a youth")
+else:
+    print("Old man")
